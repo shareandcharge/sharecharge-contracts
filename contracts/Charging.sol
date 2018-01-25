cd154f2d161302a00fa628feba68556247bbb091
Adam Staveley <adam@motionwerk.com>
2018-01-25 16:28:38 +0100

    Added use to start parameter calls

--- contracts/Charging.sol
+++ contracts/Charging.sol
@@ -2,11 +2,9 @@ pragma solidity ^0.4.18;
 
 contract Charging {
 
-  event StartEvent(bytes32 connectorId);
+  event StartEvent(bytes32 indexed connectorId, address user);
 
-  function start(bytes32 connectorId) public {
-    StartEvent(connectorId);
+  function start(bytes32 connectorId, address user) public {
+    StartEvent(connectorId, user);
   }
-
-
 }
--- test/charging.js
+++ test/charging.js
@@ -12,10 +12,18 @@ contract('Charging', (accounts) => {
 
   it('Should log the correct event details when start called', async () => {
     const connector = '0x' + crypto.randomBytes(32).toString('hex');
-    const result = await charging.start(connector);
-    const startEventListener = charging.StartEvent({}, (err, res) => {
-      assert.equal(res.args.connectorId, connector);
-      startEventListener.stopWatching();
+    const user = accounts[1];
+
+    const result = await charging.start(connector, user);
+    
+    return new Promise((resolve, reject) => {
+      const startEventListener = charging.StartEvent({}, (err, res) => {
+        const args = res.args;
+        assert.equal(args.connectorId, connector);
+        assert.equal(args.user, user);
+        startEventListener.stopWatching();      
+        resolve();
+      });  
     });
   });
 
