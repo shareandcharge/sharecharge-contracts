07070895393a547c2d60b726fc5b77a7aa7379d3
adamstaveley <adam@motionwerk.com>
2018-03-05 10:01:43 +0100

    Add docstrings to and reformat ChargingStation

--- contracts/ChargingStation.sol
+++ contracts/ChargingStation.sol
@@ -23,6 +23,18 @@ contract ChargingStation {
         _;
     }
 
+    /**
+        Constructor
+        
+        @param storageAddress   network location of StationStorage contract
+        @param coinAddress      network location of EVCoin contract
+     */
+    function ChargingStation(address storageAddress, address coinAddress) public {
+        store = StationStorage(storageAddress);
+        bank = EVCoin(coinAddress);
+    }
+
+    // HELPERS
     function bytes32ToString(bytes32 source) private pure returns (string result) {
 
         bytes memory bytesArray = new bytes(32);
@@ -40,17 +52,17 @@ contract ChargingStation {
         }
     }
 
-    function ChargingStation(address storageAddress, address coinAddress) public {
-        store = StationStorage(storageAddress);
-        bank = EVCoin(coinAddress);
-    }
-
+    // GETTERS
     function getAddr() public view returns (address) {
         return address(store);
     }
 
     function balanceOf(address user) public view returns (uint256) {
         return bank.balanceOf(user);
+    }
+
+    function isAvailable(bytes32 connectorId) public view returns (bool) {
+        return store.isAvailable(connectorId);
     }
 
     function getOwnerInformation(bytes32 id) public view returns (bytes32 client, address owner, string ownerName) {
@@ -74,18 +86,46 @@ contract ChargingStation {
         return (_price, _priceModel, _plugType, bytes32ToString(_openingHours), _isAvailable, _session);
     }
 
+    /**
+        Check whether it is necessary for a connector to be registered or updated
+
+        @params metadata        the connector metadata - compared against the properties of the Connector struct in storage
+     */
     function updateRequired(bytes32 id, bytes32 client, string ownerName, string lat, string lng, uint16 price, uint8 priceModel, uint8 plugType, string openingHours, bool isAvailable) public view returns (bool) {
         return store.updateRequired(id, client, msg.sender, stringToBytes32(ownerName),
             stringToBytes32(lat), stringToBytes32(lng), price, priceModel, plugType,
             stringToBytes32(openingHours), isAvailable);
     }
 
+    /**
+        Register or update a new connector on the network. Call updateRequired first to avoid wasting gas re-registering a connector
+
+        @params metadata        the connector metadata - will always overwrite
+     */
     function registerConnector(bytes32 id, bytes32 client, string ownerName, string lat, string lng, uint16 price, uint8 priceModel, uint8 plugType, string openingHours, bool isAvailable) public {
         store.register(id, client, msg.sender, stringToBytes32(ownerName),
             stringToBytes32(lat), stringToBytes32(lng), price, priceModel, plugType,
             stringToBytes32(openingHours), isAvailable);
     }
 
+    /**
+        Set the availability of a connector (e.g. should call if connector in use outside of blockchain)
+
+        @param clientId         the id of the client setting the availability
+        @param connectorId      the unique identifier of the connector
+        @param _isAvailable     the new availability of the connector
+     */
+    function setAvailability(bytes32 clientId, bytes32 connectorId, bool _isAvailable) public {
+        require(store.getClient(connectorId) == clientId);
+        store.setAvailability(connectorId, _isAvailable);
+    }
+
+    /**
+        Request the start of a charging session on a given connector
+
+        @param connectorId      the unique identifier of the connector to charge at
+        @param secondsToRent    the time to charge
+     */
     function requestStart(bytes32 connectorId, uint256 secondsToRent) public {
         require(store.isAvailable(connectorId) == true);
         store.setSession(connectorId, msg.sender);
@@ -95,18 +135,38 @@ contract ChargingStation {
         StartRequested(clientId, connectorId, msg.sender, secondsToRent);
     }
 
+    /**
+        Confirm the start of a charging session at a given connector
+
+        @modifier stationOwnerOnly      callable only by the owner of the connector
+        
+        @param connectorId      the unique identifier of the connector to confirm
+        @param controller       the address of the driver requesting the start
+     */
     function confirmStart(bytes32 connectorId, address controller) public stationOwnerOnly(connectorId) {
         require(store.getSession(connectorId) == controller);
         store.setAvailability(connectorId, false);
         StartConfirmed(connectorId);
     }
 
+    /**
+        Request to stop a current charging session
+
+        @param connectorId      the unique identifier of the connector to stop
+     */
     function requestStop(bytes32 connectorId) public {
         require(store.getSession(connectorId) == msg.sender);
         bytes32 clientId = store.getClient(connectorId);
         StopRequested(clientId, connectorId, msg.sender);
     }
 
+    /**
+        Confirm a stop on a current charging session
+
+        @modifier stationOwnerOnly      callable only by the owner of the connector
+
+        @param connectorId      the unique identifier of the connector
+     */
     function confirmStop(bytes32 connectorId) public stationOwnerOnly(connectorId) {
         store.setSession(connectorId, 0);
         store.setAvailability(connectorId, true);
@@ -114,15 +174,14 @@ contract ChargingStation {
         StopConfirmed(connectorId);
     }
 
-    function isAvailable(bytes32 connectorId) public view returns (bool) {
-        return store.isAvailable(connectorId);
-    }
+    /**
+        Report an error with a charging session at a connector
 
-    function setAvailability(bytes32 clientId, bytes32 connectorId, bool _isAvailable) public {
-        require(store.getClient(connectorId) == clientId);
-        store.setAvailability(connectorId, _isAvailable);
-    }
+        @modifier stationOwnerOnly      callable only by the owner of the connector
 
+        @param connectorId      the unique identifier of the connector
+        @param errorCode        the type of error (0 = start error; 1 = stop error)
+     */
     function logError(bytes32 connectorId, uint8 errorCode) public stationOwnerOnly(connectorId) {
         if (errorCode == 0) {
             bank.transfer(store.getSession(connectorId), 1);
