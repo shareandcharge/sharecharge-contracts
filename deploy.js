fc0a540d85fef005b4e2d58a9300b58dc1dedf08
adamstaveley <adam@motionwerk.com>
2018-03-06 11:24:40 +0100

    uncomment deploy script

--- deploy.js
+++ deploy.js
@@ -6,7 +6,7 @@ const fs = require("fs");
 // change this to your needs
 const pathToCoreClientLib = process.env.LIB;
 
-console.log("Deploy");
-console.log(pathToCoreClientLib);
-// fs.writeFileSync(pathToCoreClientLib + "/src/config/ChargingStation.json",
-//     JSON.stringify(config.ChargingStation, null, 2));
+console.log("Deploy to", pathToCoreClientLib);
+
+fs.writeFileSync(pathToCoreClientLib + "/src/config/ChargingStation.json",
+    JSON.stringify(config.ChargingStation, null, 2));
