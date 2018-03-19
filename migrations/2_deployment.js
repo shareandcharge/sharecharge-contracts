a7ecd2108908bf4ca9f167539676a1918f048cf8
Sebastian Gerske <sebastian.gerske@digitalunit.berlin>
2018-03-19 12:19:39 +0100

    hardened deployment of ouput file

--- migrations/2_deployment.js
+++ migrations/2_deployment.js
@@ -6,23 +6,29 @@ const Charging = artifacts.require("./Charging.sol");
 
 
 module.exports = async function (deployer) {
-  // Use deployer to state migration tasks.
-  await deployer.deploy(StationStorage);
-  await deployer.deploy(ConnectorStorage);
-  await deployer.deploy(Charging, ConnectorStorage.address);
+    // Use deployer to state migration tasks.
+    await deployer.deploy(StationStorage);
+    await deployer.deploy(ConnectorStorage);
+    await deployer.deploy(Charging, ConnectorStorage.address);
 
-  let config = {};
-  const contracts = [Charging, StationStorage, ConnectorStorage];
-  contracts.forEach(contract => {
-    config[contract.contractName] = {
-      abi: contract.abi,
-      address: contract.address,
-      bytecode: contract.bytecode
-    };
-  });
+    let config = {};
+    const contracts = [Charging, StationStorage, ConnectorStorage];
+    contracts.forEach(contract => {
+        config[contract.contractName] = {
+            abi: contract.abi,
+            address: contract.address,
+            bytecode: contract.bytecode
+        };
+    });
 
-  const outputPath = process.env['HOME'] + '/.sharecharge/config.json';
-  await fs.writeFile(outputPath, JSON.stringify(config, null, 2), err => {
-    if (err) console.log(err)
-  });
+    const path = process.env['HOME'] + '/.sharecharge/';
+
+    if (!fs.existsSync(path)) {
+        fs.mkdirSync(path);
+    }
+
+    const outputPath = path + 'config.json';
+    await fs.writeFile(outputPath, JSON.stringify(config, null, 2), err => {
+        if (err) console.log(err);
+    });
 };
