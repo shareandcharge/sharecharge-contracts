9994031d00957de25cc88e6ecb90391a970477e8
adamstaveley <adam@motionwerk.com>
2018-04-29 20:33:49 +0200

    connect copy of charging contract to external storage

--- contracts/ExternalStorage.sol
+++ contracts/ExternalStorage.sol
@@ -6,28 +6,33 @@ contract ExternalStorage is Ownable {
 
     struct ChargePointOperator {
         mapping(bytes32 => bytes32) locations;
-        bytes32[] globalIds;
+        bytes32[] scIds;
         bytes32 tariffs;
     }
 
     mapping(address => ChargePointOperator) private CPOs;
-    // mapping(bytes32 => address) private ownerOf;
+    mapping(bytes32 => address) private ownerOf;
 
-    event LocationAdded(bytes32 globalId);
-    event LocationUpdated(bytes32 globalId);
+    event LocationAdded(bytes32 scId);
+    event LocationUpdated(bytes32 scId);
+    event EvseAvailabilityUpdated(bytes32 scId, bytes32 evseId, bool isAvailable);
 
-    function addLocation(bytes32 globalId, bytes32 externalHash) public {
-        require(CPOs[msg.sender].locations[globalId] == bytes32(0));
-        CPOs[msg.sender].locations[globalId] = externalHash;
-        CPOs[msg.sender].globalIds.push(globalId);
-        // ownerOf[globalID] = msg.sender;
-        emit LocationAdded(globalId);
+    function addLocation(bytes32 scId, bytes32 externalHash) public {
+        require(CPOs[msg.sender].locations[scId] == bytes32(0));
+        CPOs[msg.sender].locations[scId] = externalHash;
+        CPOs[msg.sender].scIds.push(scId);
+        ownerOf[scId] = msg.sender;
+        emit LocationAdded(scId);
     }
 
-    function updateLocation(bytes32 globalId, bytes32 newHash) public {
-        require(CPOs[msg.sender].locations[globalId] != bytes32(0));
-        CPOs[msg.sender].locations[globalId] = newHash;
-        emit LocationUpdated(globalId);
+    function updateLocation(bytes32 scId, bytes32 newHash) public {
+        require(CPOs[msg.sender].locations[scId] != bytes32(0));
+        CPOs[msg.sender].locations[scId] = newHash;
+        emit LocationUpdated(scId);
+    }
+
+    function updateEvseAvailability(bytes32 scId, bytes32 evseId, bool isAvailable) public {
+        emit EvseAvailabilityUpdated(scId, evseId, isAvailable);
     }
 
     function addTariffs(bytes32 externalHash) public {
@@ -40,12 +45,16 @@ contract ExternalStorage is Ownable {
         CPOs[msg.sender].tariffs = newHash;
     }
 
-    function getLocationById(address cpo, bytes32 globalId) view public returns (bytes32) {
-        return CPOs[cpo].locations[globalId];
+    function getLocationById(address cpo, bytes32 scId) view public returns (bytes32) {
+        return CPOs[cpo].locations[scId];
     }
 
-    function getGlobalIdsByCPO(address cpo) view public returns (bytes32[]) {
-        return CPOs[cpo].globalIds;
+    function getOwnerById(bytes32 scId) view public returns (address) {
+        return ownerOf[scId];
+    }
+
+    function getShareAndChargeIdsByCPO(address cpo) view public returns (bytes32[]) {
+        return CPOs[cpo].scIds;
     }
 
     function getTariffsByCPO(address cpo) view public returns (bytes32) {
---
+++ test/charging2.js
@@ -0,0 +1,119 @@
+const Charging = artifacts.require('./Charging2.sol');
+const ExternalStorage = artifacts.require('./ExternalStorage.sol');
+const MSPToken = artifacts.require('./MSPToken.sol');
+
+const helpers = require('./helpers');
+
+contract('Charging2', function (accounts) {
+
+    let store, token, charging;
+
+    beforeEach(async () => {
+        store = await ExternalStorage.new();
+        token = await MSPToken.new("MSPToken", "MSP");
+        charging = await Charging.new();
+        charging.setStorageAddress(store.address);
+        // store.setAccess(charging.address);
+        token.setAccess(charging.address);
+    });
+
+
+    async function addLocation(owner) {
+        const scId = helpers.randomBytes32String();
+        const evseId = "FR138E1ETG5578567YU8D";
+        await store.addLocation(scId, evseId, { from: owner });
+        return { scId, evseId };
+    }
+
+    it('should not allow request start on unregistered location', async () => {
+        await token.mint(accounts[1], 500);
+        const id = () => helpers.randomBytes32String();
+        try {
+            await charging.requestStart(id(), id(), token.address, 100, { from: accounts[1] });
+            expect.fail();
+        } catch (err) {
+            expect(err.message.search('revert') != -1).to.equal(true);            
+        }
+    });
+
+    it('should only allow owner to call confirmation functions', async () => {
+        await token.mint(accounts[1], 500);
+        const { scId, evseId } = await addLocation(accounts[0]);
+        await charging.requestStart(scId, evseId, token.address, 100, { from: accounts[1] });
+        try {
+            await charging.confirmStart(scId, evseId, helpers.randomBytes32String(), { from: accounts[1] });
+            expect.fail();
+        } catch (err) {
+            expect(err.message.search('revert') != -1).to.equal(true);
+        }
+    });
+
+    it('should emit charge detail record event at end of charging session', async () => {
+
+        await token.mint(accounts[1], 500);
+
+        const balanceBefore = await token.balanceOf(accounts[1]);
+        expect(balanceBefore.toNumber()).to.equal(500);
+        
+        const { scId, evseId } = await addLocation(accounts[0]);
+        
+        const requestStart = await charging.requestStart(scId, evseId, token.address, 200, { from: accounts[1] });
+        console.log('requestStart gas:', requestStart.receipt.gasUsed);
+
+        const allowance = await token.allowance(accounts[1], charging.address);
+        expect(allowance.toNumber()).to.equal(200);
+
+        const sessionId = helpers.randomBytes32String();
+        const confirmStart = await charging.confirmStart(scId, evseId, sessionId, { from: accounts[0] });
+        console.log('confirmStart gas:', confirmStart.receipt.gasUsed);
+
+        // check allowance has been expended after token transfer
+        const allowance2 = await token.allowance(accounts[1], charging.address);
+        expect(allowance2.toNumber()).to.equal(0);
+
+        // charged 10kw over 2 hours
+        const confirmStop = await charging.confirmStop(scId, evseId, { from: accounts[0] });
+        console.log('confirmStop gas:', confirmStop.receipt.gasUsed);
+        
+        const cdr = await charging.chargeDetailRecord(scId, evseId, 200, Date.now());
+        console.log('chargeDetailRecord gas:', cdr.receipt.gasUsed);
+
+        const balanceAfter = await token.balanceOf(accounts[1]);
+        expect(balanceAfter.toNumber()).to.equal(300);
+
+        const balanceEvseOwner = await token.balanceOf(accounts[0]);
+        expect(balanceEvseOwner.toNumber()).to.equal(200);
+
+        const cdrEvent = cdr.logs.filter(log => log.event === 'ChargeDetailRecord');
+        expect(cdrEvent.length).to.equal(1);
+        expect(cdrEvent[0].args.controller).to.equal(accounts[1]);
+        expect(cdrEvent[0].args.finalPrice.toNumber()).to.equal(200);    
+    });
+
+    it('Should correctly refund driver tokens upon early stop', async () => {
+        await token.mint(accounts[1], 500);
+        const { scId, evseId } = await addLocation(accounts[0]);
+
+        await charging.requestStart(scId, evseId, token.address, 300, { from: accounts[1] });
+
+        const sessionId = helpers.randomBytes32String();
+        await charging.confirmStart(scId, evseId, sessionId, { from: accounts[0] });        
+
+        const balanceControllerBefore = await token.balanceOf(accounts[1]);
+        expect(balanceControllerBefore.toNumber()).to.equal(200);
+
+        await charging.confirmStop(scId, evseId, { from: accounts[0] });
+        
+        const result = await charging.chargeDetailRecord(scId, evseId, 225, Date.now());
+        
+        const balanceControllerAfter = await token.balanceOf(accounts[1]);
+        expect(balanceControllerAfter.toNumber()).to.equal(275);
+        const balanceEvseOwner = await token.balanceOf(accounts[0]);
+        expect(balanceEvseOwner.toNumber()).to.equal(225);
+
+        const cdrEvent = result.logs.filter(log => log.event === 'ChargeDetailRecord');
+        expect(cdrEvent.length).to.equal(1);
+        expect(cdrEvent[0].args.finalPrice.toNumber()).to.equal(225); 
+    });
+
+});
