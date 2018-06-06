869670baf387017764b8c18db4ab1667ea57bffe
adamstaveley <adam@motionwerk.com>
2018-06-06 11:02:30 +0200

    0.3.1: updated pilot contracts with sessionId string, revert comments

--- migrations/5_wireup.js
+++ migrations/5_wireup.js
@@ -4,7 +4,9 @@ const Charging = artifacts.require("./Charging.sol");
 
 module.exports = async function(deployer) {
     const storage = await ExternalStorage.deployed();
+    const token = await MSPToken.deployed();
     const charging = await Charging.deployed();
     charging.setStorageAddress(storage.address);
+    token.setAccess(charging.address);
     // console.log(`set storage [${storage.address}] in charging [${charging.address}]`);
 };
