eb1f309cbc9a261abc29bbddae1049ac4878d9c1
Andrei <danandreist@gmail.com>
2018-07-18 12:53:34 +0300

    new pilot defs

--- truffle-config.js
+++ truffle-config.js
@@ -13,8 +13,10 @@ module.exports = {
             gas: 4000000
         },
         pilot: {
-            host: 'localhost',
+            host: '52.59.207.123',
             port: 8545,
+            from: "0x00bd138abd70e2f00903268f3db08f2d25677c9e",
+            gasPrice: 0,
             network_id: "*",
             gas: 4000000
         },
