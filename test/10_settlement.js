4d4576194bc5d0d4677a21c9ae38a683f132daa2
Adam Staveley <31211028+adamstaveley@users.noreply.github.com>
2019-02-04 16:09:11 +0100

    Platform v1 settlement security (#9)
    * new contracts before each test case
    
    * make proxy transfer non repeatable

--- test/10_settlement.js
+++ test/10_settlement.js
@@ -25,10 +25,11 @@ contract('Settlement', function (accounts) {
   let settlement, token
   const toBN = (num) => web3.utils.toBN(new BigNumber(num).toString(10))
   const privateKey = '0x49b2e2b48cfc25fda1d1cbdb2197b83902142c6da502dcf1871c628ea524f11b'
+  const sessionId = 'c2d7f3a7-ed43-4780-8e86-ab73654e081a'
 
   const wallet = new ethers.Wallet(privateKey)
 
-  before(async () => {
+  beforeEach(async () => {
     settlement = await Settlement.new()
     token = await TestToken.new()
   })
@@ -40,6 +41,8 @@ contract('Settlement', function (accounts) {
   })
 
   it('does hold the correct balance after provisioning the account', async () => {
+    await token.mint(wallet.address, toBN('1e29'))
+
     await token.approve(settlement.address, toBN('5e28'), {from: wallet.address})
     assert.equal((await token.allowance(wallet.address, settlement.address)).toString(), toBN('5e28').toString())
 
@@ -49,26 +52,53 @@ contract('Settlement', function (accounts) {
   })
 
   it('can transfer tokens given a signed transfer message', async () => {
+    await token.mint(wallet.address, toBN('1e29'))
+    await token.approve(settlement.address, toBN('5e28'), {from: wallet.address})
+    await settlement.transferInto(wallet.address, toBN('5e28'), token.address, {from: wallet.address})
+
     let sig = await sign(accounts[1], '2e28', token.address, wallet)
 
-    await settlement.transfer(accounts[1], toBN('2e28'), token.address, sig.v, sig.r, sig.s, {from: accounts[2]})
+    const tx = await settlement.transfer(accounts[1], sessionId, toBN('2e28'), token.address, sig.v, sig.r, sig.s, {from: accounts[2]})
     assert.equal((await token.balanceOf(accounts[1])).toString(), toBN('2e28').toString())
     assert.equal((await settlement.tokenBalances(token.address, wallet.address)).toString(), toBN('3e28').toString())
 
+    console.log('transfer gasUsed:', tx.receipt.gasUsed)
   })
 
   it('reverts the transfer on insufficient funds', async () => {
+    await token.mint(wallet.address, toBN('1e29'))
+    await token.approve(settlement.address, toBN('5e28'), {from: wallet.address})
+    await settlement.transferInto(wallet.address, toBN('5e28'), token.address, {from: wallet.address})
+
     let sig = await sign(accounts[1], '2e29', token.address, wallet)
 
     try {
-      await settlement.transfer(accounts[1], toBN('2e29'), token.address, sig.v, sig.r, sig.s, {from: accounts[2]})
+      await settlement.transfer(accounts[1], sessionId, toBN('2e29'), token.address, sig.v, sig.r, sig.s, {from: accounts[2]})
       assert.fail()
     } catch (e) {
       assert.equal(e.message, 'Returned error: VM Exception while processing transaction: revert')
     }
+    assert.equal((await token.balanceOf(accounts[1])).toString(), toBN('0e28').toString())
+    assert.equal((await settlement.tokenBalances(token.address, wallet.address)).toString(), toBN('5e28').toString())
+
+  })
+
+  it('reverts the transfer if previously executed for a given session id', async () => {
+    await token.mint(wallet.address, toBN('1e29'))
+    await token.approve(settlement.address, toBN('5e28'), {from: wallet.address})
+    await settlement.transferInto(wallet.address, toBN('5e28'), token.address, {from: wallet.address})
+
+    let sig = await sign(accounts[1], '2e28', token.address, wallet)
+
+    try {
+      await settlement.transfer(accounts[1], sessionId, toBN('2e28'), token.address, sig.v, sig.r, sig.s, {from: accounts[2]})
+      await settlement.transfer(accounts[1], sessionId, toBN('2e28'), token.address, sig.v, sig.r, sig.s, {from: accounts[2]})
+      assert.fail()
+    } catch (e) {
+      assert.equal(e.message.startsWith('Returned error: VM Exception while processing transaction: revert'), true)
+    }
     assert.equal((await token.balanceOf(accounts[1])).toString(), toBN('2e28').toString())
     assert.equal((await settlement.tokenBalances(token.address, wallet.address)).toString(), toBN('3e28').toString())
-
   })
 
 })
