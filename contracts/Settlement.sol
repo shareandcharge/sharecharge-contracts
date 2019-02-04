a5266810355ce4b5aa2b1bbe21350664efdfa695
adamstaveley <adam@motionwerk.com>
2019-02-04 11:37:32 +0100

    make proxy transfer non repeatable

--- contracts/Settlement.sol
+++ contracts/Settlement.sol
@@ -7,15 +7,17 @@ import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 contract Settlement is Ownable {
     using SafeMath for uint256;
 
-    struct Withdrawal {
-        uint256 amount;
-        address token;
-        uint256 timestamp;
-    }
+    // struct Withdrawal {
+    //     uint256 amount;
+    //     address token;
+    //     uint256 timestamp;
+    // }
 
     // token => (holder => balance)
     mapping(address => mapping(address => uint256)) public tokenBalances;
-    mapping(address => Withdrawal) withdrawals;
+    // signer => (sessionId => withdrawn)
+    mapping(address => mapping(string => bool)) private withdrawals;
+    // mapping(address => Withdrawal) withdrawals;
 
     // the owner can transfer arbitrary token amounts. this should only be used in extreme cases
     function transferTokens(address recipient, uint256 amount, address token) public onlyOwner {
@@ -31,13 +33,14 @@ contract Settlement is Ownable {
     }
 
     string constant private prefix = "\u0019Ethereum Signed Message:\n32";
-    function transfer(address to, uint256 amount, address token, uint8 v, bytes32 r, bytes32 s)
+    function transfer(address to, string memory sessionId, uint256 amount, address token, uint8 v, bytes32 r, bytes32 s)
     public {
         bytes32 paramHash = keccak256(abi.encodePacked(to, amount, token));
         address signer = ecrecover(keccak256(abi.encodePacked(prefix, paramHash)), v, r, s);
+        require(withdrawals[signer][sessionId] == false, "Funds already withdrawn for session with that ID");
         //SafeMath ensures that the signer has enough tokens in their payment account
         tokenBalances[token][signer] = tokenBalances[token][signer].sub(amount);
-
+        withdrawals[signer][sessionId] = true;
         IERC20(token).transfer(to, amount);
     }
 
--- test/10_settlement.js
+++ test/10_settlement.js
@@ -25,6 +25,7 @@ contract('Settlement', function (accounts) {
   let settlement, token
   const toBN = (num) => web3.utils.toBN(new BigNumber(num).toString(10))
   const privateKey = '0x49b2e2b48cfc25fda1d1cbdb2197b83902142c6da502dcf1871c628ea524f11b'
+  const sessionId = 'c2d7f3a7-ed43-4780-8e86-ab73654e081a'
 
   const wallet = new ethers.Wallet(privateKey)
 
@@ -57,10 +58,11 @@ contract('Settlement', function (accounts) {
 
     let sig = await sign(accounts[1], '2e28', token.address, wallet)
 
-    await settlement.transfer(accounts[1], toBN('2e28'), token.address, sig.v, sig.r, sig.s, {from: accounts[2]})
+    const tx = await settlement.transfer(accounts[1], sessionId, toBN('2e28'), token.address, sig.v, sig.r, sig.s, {from: accounts[2]})
     assert.equal((await token.balanceOf(accounts[1])).toString(), toBN('2e28').toString())
     assert.equal((await settlement.tokenBalances(token.address, wallet.address)).toString(), toBN('3e28').toString())
 
+    console.log('transfer gasUsed:', tx.receipt.gasUsed)
   })
 
   it('reverts the transfer on insufficient funds', async () => {
@@ -71,7 +73,7 @@ contract('Settlement', function (accounts) {
     let sig = await sign(accounts[1], '2e29', token.address, wallet)
 
     try {
-      await settlement.transfer(accounts[1], toBN('2e29'), token.address, sig.v, sig.r, sig.s, {from: accounts[2]})
+      await settlement.transfer(accounts[1], sessionId, toBN('2e29'), token.address, sig.v, sig.r, sig.s, {from: accounts[2]})
       assert.fail()
     } catch (e) {
       assert.equal(e.message, 'Returned error: VM Exception while processing transaction: revert')
@@ -79,6 +81,24 @@ contract('Settlement', function (accounts) {
     assert.equal((await token.balanceOf(accounts[1])).toString(), toBN('0e28').toString())
     assert.equal((await settlement.tokenBalances(token.address, wallet.address)).toString(), toBN('5e28').toString())
 
+  })
+
+  it('reverts the transfer if previously executed for a given session id', async () => {
+    await token.mint(wallet.address, toBN('1e29'))
+    await token.approve(settlement.address, toBN('5e28'), {from: wallet.address})
+    await settlement.transferInto(wallet.address, toBN('5e28'), token.address, {from: wallet.address})
+
+    let sig = await sign(accounts[1], '2e28', token.address, wallet)
+
+    try {
+      await settlement.transfer(accounts[1], sessionId, toBN('2e28'), token.address, sig.v, sig.r, sig.s, {from: accounts[2]})
+      await settlement.transfer(accounts[1], sessionId, toBN('2e28'), token.address, sig.v, sig.r, sig.s, {from: accounts[2]})
+      assert.fail()
+    } catch (e) {
+      assert.equal(e.message.startsWith('Returned error: VM Exception while processing transaction: revert'), true)
+    }
+    assert.equal((await token.balanceOf(accounts[1])).toString(), toBN('2e28').toString())
+    assert.equal((await settlement.tokenBalances(token.address, wallet.address)).toString(), toBN('3e28').toString())
   })
 
 })
